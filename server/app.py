f9a054e2351f17dcd7535642659159380992a37c
Elvis-785 <kiruielvis3358@gmail.com>
2025-02-20 21:03:48 +0300

    updated the file

--- server/app.py
+++ server/app.py
@@ -13,49 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 
-"""## Instructions
-
-For our basic login feature, we'll need the following functionality:
-
-- A user can log in by providing their username in a form.
-- A user can log out.
-- A user can remain logged in, even after refreshing the page.
-
-We'll need to create the resources to handle each of these features. Let's get
-started!
-
-> **_NOTE: This lab uses the Flask-Restful module rather than vanilla Flask. You
-> do not need to use it to pass the tests, but we recommend giving it a shot._**
-
-### Sessions
-
-- Generate these resources:
-
-- `Login` is located at `/login`.
-
-  - It has one route, `post()`.
-  - `post()` gets a `username` from `request`'s JSON.
-  - `post()` retrieves the user by `username` (we made these unique for you).
-  - `post()` sets the session's `user_id` value to the user's `id`.
-  - `post()` returns the user as JSON with a 200 status code.
-
-- `Logout` is located at `/logout`.
-
-  - It has one route, `delete()`.
-  - `delete()` removes the `user_id` value from the session.
-  - `delete()` returns no data and a 204 (No Content) status code.
-
-- `CheckSession` is located at `/check_session`.
-  - It has one route, `get()`.
-  - `get()` retrieves the `user_id` value from the session.
-  - If the session has a `user_id`, `get()` returns the user as JSON with a 200
-    status code.
-  - If the session does not have a `user_id`, `get()` returns no data and a 401
-    (Unauthorized) status code.
-
----
-"""
-
 migrate = Migrate(app, db)
 
 db.init_app(app)
